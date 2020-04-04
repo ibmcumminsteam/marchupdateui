eefe5db6e4c6d19fd43473990ac6c4bd6a6e33de
ibmcumminsteam <56838139+ibmcumminsteam@users.noreply.github.com>
2020-04-04 11:24:06 +0530

    Update System.js

--- allscreens/System.js
+++ allscreens/System.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView, Text, } from 'react-native';
-import MyOtherHeader from '../MyOtherHeader';
+import MyOtherHeader from '../screens/MyOtherHeader';
  
 export default class System extends Component{
 
@@ -13,7 +13,7 @@ export default class System extends Component{
           <><MyOtherHeader/>
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
               
-          <Text>Under Deployment</Text>
+          <Text>Under Development</Text>
           </View>
           </>
           
