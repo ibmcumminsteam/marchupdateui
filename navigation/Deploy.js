7d1aee58a5120feb9f8830d01ec357dac1e7e199
ibmcumminsteam <56838139+ibmcumminsteam@users.noreply.github.com>
2020-04-04 11:28:10 +0530

    Update Deploy.js

--- navigation/Deploy.js
+++ navigation/Deploy.js
@@ -1,17 +1,12 @@
-
-// Profile screen
 import React, { Component } from 'react';
-//import react in our code.
 import { Text, View } from 'react-native';
-//import all the components we are going to use.
 
 export default class Profile extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Under Deployment</Text>
+        <Text>Under Development</Text>
       </View>
     );
   }
 }
-
