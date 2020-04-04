3e214a32f5556d75d1d684262e235212dca6a436
ibmcumminsteam <56838139+ibmcumminsteam@users.noreply.github.com>
2020-04-04 11:18:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,136 +1,113 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
+  import 'react-native-gesture-handler';
+  import React from 'react';
+  import { createAppContainer,  createSwitchNavigator } from 'react-navigation';
+  import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
+  import {createStackNavigator} from 'react-navigation-stack';
+  import { createDrawerNavigator } from 'react-navigation-drawer';
+  import AdministrationConsole from './AdministrationConsole';
+  import ClusterOverview from './navigation/ClusterOverview';
+  import Deploy from './navigation/Deploy';
+  import GettingStarted from './navigation/GettingStarted';
+  import Login from './Login';
+  import MyHeader from './screens/MyHeader';
+  import Screen2 from './Screen2';
+  import CloudScreen from './allscreens/CloudScreen';
+  import Catalog from './allscreens/Catalog';
+  import Hardware from './allscreens/Hardware';
+  import System from './allscreens/System';
 
-import { createAppContainer,  createSwitchNavigator } from 'react-navigation';
-import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
-import {createStackNavigator} from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-
-import AdministrationConsole from './AdministrationConsole';
-import ClusterOverview from './navigation/ClusterOverview';
-import Deploy from './navigation/Deploy';
-import GettingStarted from './navigation/GettingStarted';
-import Login from './Login';
-import MyHeader from './screens/MyHeader';
-import Screen2 from './Screen2';
-import CloudScreen from './allscreens/CloudScreen';
-import Catalog from './allscreens/Catalog';
-import Hardware from './allscreens/Hardware';
-import System from './allscreens/System';
-
-
-
-const TabNavigation = createMaterialTopTabNavigator(
+  const TabNavigation = createMaterialTopTabNavigator(
   {
-    GettingStarted : {screen : GettingStarted, navigationOptions: {drawerLockMode: "locked-closed"}},
-    Deploy : {screen : Deploy},
-    ClusterOverview : { screen: ClusterOverview },
-    // AdministrationConsole : { screen: AdministrationConsole },
-    
-    
-    
-
+  GettingStarted : {screen : GettingStarted, navigationOptions: {drawerLockMode: "locked-closed"}},
+  Deploy : {screen : Deploy},
+  ClusterOverview : { screen: ClusterOverview },
   },
   {
-    tabBarPosition: 'top',
-    swipeEnabled: true,
-    animationEnabled: true,
-    tabBarOptions: {
-      activeTintColor: '#FFFFFF',
-      inactiveTintColor: '#F8F8F8',
-      style: {
-        backgroundColor: '#14132f',
-        
-      },
-      labelStyle: {
-        textAlign: 'center',
-        fontSize: 10
-      },
-      indicatorStyle: {
-        borderBottomColor: '#87B56A',
-        borderBottomWidth: 6,
-      },
-    },
+  tabBarPosition: 'top',
+  swipeEnabled: true,
+  animationEnabled: true,
+  tabBarOptions: {
+  activeTintColor: '#FFFFFF',
+  inactiveTintColor: '#F8F8F8',
+  style: {
+  backgroundColor: '#14132f',
+
+  },
+  labelStyle: {
+  textAlign: 'center',
+  fontSize: 10
+  },
+  indicatorStyle: {
+  borderBottomColor: '#87B56A',
+  borderBottomWidth: 6,
+  },
+  },
   }
-);
+  );
 
-
-//making a StackNavigator to export as default
-const StackNavigation = createStackNavigator({
+  const StackNavigation = createStackNavigator({
   T1: {
-    screen: TabNavigation,
-    navigationOptions: {
-      header: <MyHeader/>
-    
-    },
+  screen: TabNavigation,
+  navigationOptions: {
+  header: <MyHeader/>
+
+  },
   }, 
   Login : {screen: Login, navigationOptions:{header: null, headerRight:{icon: 'home',} }}
-},
-{  
+  },
+  {  
   initialRouteName: "Login"  ,
   navigationOptions :{
-    drawerLabel: () => null,
-    header: null,
-      drawerLockMode: "locked-closed",
-      disableGestures: true
-}
-} ,
-);
+  drawerLabel: () => null,
+  header: null,
+  drawerLockMode: "locked-closed",
+  disableGestures: true
+  }
+  } ,
+  );
 
 
-const DrawerNavigation = createDrawerNavigator({
+  const DrawerNavigation = createDrawerNavigator({
 
- Login:{ screen : StackNavigation , navigationOptions:{ header: null,
-      // drawerLockMode: "locked-closed",
-      }},
-      AdministrationConsole: {screen: AdministrationConsole, navigationOptions:{ header: null,
-        }},
-        
-       
-      Patterns: {
-        screen: Screen2
-      },
-      Catalog: {
-        screen: Catalog
-      },
-      Cloud: {
-        screen: CloudScreen
-      },
-      System: {
-        screen: System
-      }, 
-      Hardware: {
-        screen: Hardware
-      },
-      
-
-  // Home: { screen: Home },
-  // Contacts: { screen: Contact },
-
-},
-);
+  Login:{ screen : StackNavigation , navigationOptions:{ header: null,
+  }},
+  AdministrationConsole: {screen: AdministrationConsole, navigationOptions:{ header: null,
+  }},
 
 
+  Patterns: {
+  screen: Screen2
+  },
+  Catalog: {
+  screen: Catalog
+  },
+  Cloud: {
+  screen: CloudScreen
+  },
+  System: {
+  screen: System
+  }, 
+  Hardware: {
+  screen: Hardware
+  },
+  },
+  );
 
+  const MainNav = createSwitchNavigator({
 
-const MainNav = createSwitchNavigator({
-
-  
   HomeDrawer : DrawerNavigation,  
   AuthStack : StackNavigation,
 
-});
+  });
 
-const AppContainer = createAppContainer(MainNav);
+  const AppContainer = createAppContainer(MainNav);
 
-// const AppContainer = createAppContainer(DrawerAppNavigator);  
-export default class App extends React.Component {  
- render() {  
-      return (
-      <>
-      
-      <AppContainer/>
-      </>
-      ) 
+  export default class App extends React.Component {  
+  render() {  
+  return (
+  <>
+  <AppContainer/>
+  </>
+  ) 
   }  
-}
+  }
